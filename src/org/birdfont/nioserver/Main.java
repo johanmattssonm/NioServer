d8d2eff35215e70f4f681bca8302fa5e56fc811f
Johan Mattsson <johan.mattsson.m@gmail.com>
2017-04-13 13:49:58 +0200

    Setup instructions

--- src/org/birdfont/nioserver/Main.java
+++ src/org/birdfont/nioserver/Main.java
@@ -1,6 +1,7 @@
 package org.birdfont.nioserver;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /** A small server using java nio framwork and ssl
  * @author Johan Mattsson
@@ -10,7 +11,18 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Server server = new Server();
+			
+			// TODO: load a keystore to the ssl service, you can use Java Keytool to generate the key
+			InputStream keyStore = null;
+			
+			if (keyStore == null) {
+				System.err.println("No keystore has been added to the project.");
+				return;
+			}
+			
+			server.setKeyStore(keyStore, "testkeys");
 			server.setup();
+			
 			server.start();
 		} catch (IOException e) {
 			e.printStackTrace();
